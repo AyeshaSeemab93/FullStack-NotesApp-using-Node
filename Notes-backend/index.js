8ab74be755929b3ad858c3e415a7e493c59180be
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-11-29 11:48:46 +0200

    Add the database work into index.js

--- Notes-backend/index.js
+++ Notes-backend/index.js
@@ -1,50 +1,104 @@
 // const http = require('http') instaead we use express now
 const express = require('express')
-//creating server
-const app = express();
-//for posting (json parser: to access the raw data sent with req)
-app.use(express.json())
-//to connect frontend + backend to 1 place
-const cors = require('cors')
+const app = express(); //creating server
+app.use(express.json()) //for posting (json parser: to access the raw data sent with req)
+
+const cors = require('cors')  //to connect frontend + backend to 1 place
 app.use(cors());
 
-//to show static content brought with dist folder
-app.use(express.static('dist'))
+app.use(express.static('dist')) //to show static content brought with dist folder
+const mongoose = require("mongoose") //to communicate with mongoDB database
 
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
+ //ask for password in terminal
+if(process.argv.length <3){
+  console.log("give password as argument");
+  process.exit;
+}
+const password = process.argv[2];
+
+//connect to mongoDB
+const url = `mongodb+srv://Ayesha:${password}@cluster0.ekh6jf2.mongodb.net/databaseName?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false)  // Set 'strictQuery' to false, relaxing query validation
+mongoose.connect(url);
+
+//create schema(design of data)
+const noteSchema = new mongoose.Schema({
+content: String,
+important: Boolean,
+});
+// this step is to delete _id(object) and create id(string) from _id before converting to json
+noteSchema.set('toJSON',{
+  transform: (document, returnedObject) =>{
+    returnedObject.id = document._id.toString();
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+//create model(name of table, design to store)
+const Note = mongoose.model('Note', noteSchema);
 
 let notes = [
-  {
-    id: 1,
-    content: "HTML backend indexedDB.js easy",
-    important: true
-  },
-  {
-    id: 2,
-  
-    content: "backend indexedDB.js,Browser can execute only JavaScript",
-    important: false
-  },
-  
-  {
-    id: 3,
-    content: "backend indexedDB.js,GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+    {
+      id: 1,
+      content: "HTML backend indexedDB.js easy",
+      important: true
+    },
+    {
+      id: 2,
+    
+      content: "backend indexedDB.js,Browser can execute only JavaScript",
+      important: false
+    },
+    
+    {
+      id: 3,
+      content: "backend indexedDB.js,GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+];
 
+//create and save each note 1 by 1
+      function addNote(content, important) {
+        const note = new Note({
+          content,
+          important,
+          });
+        return note.save().then(result => {
+          console.log('Note saved:', result);
+        });
+      }
+ // Using Promise.all to wait for all notes to be saved before closing the connection
+      const promiseArray = notes.map(note => addNote(note.content, note.important));
+      Promise.all(promiseArray)
+        .then(() => {
+          console.log('All notes saved successfully');
+        })
+        .catch(error => {
+          console.error('Error saving notes:', error);
+        });
 
 app.get('/', (req, res)=>{
   console.log('request received');
   res.send('<h1>Hello World! Welcome to notes(Node-Express part 3)</h1>')
 })
+
 app.get('/api/notes', (req, res)=>{
   console.log('request for all notes');
-  res.json(notes);
+  //res.json(notes); directly show notes from the handcoded above notes
+  //find notes from database 
+  Note.find({})
+  .then(notes=>{
+    console.log("found")
+
+    res.json(notes) //notes coming from database are array(DB documents).we convert then to json strings before sending them to client
+  })
+  .catch(error=>{
+    console.log("Error in fetching data");
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  })
 })
+
 app.get('/api/notes/:id', (req, res) =>{
   console.log('in get method')
   const id = Number(req.params.id);
@@ -95,11 +149,10 @@ app.post('/api/notes', (request, response) => {
 
 
 
-
-
 //running the servere (for fly.io, render deploying)
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`)
-  
-})
+
+});
--- Notes-backend/mongo.js
+++ Notes-backend/mongo.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const mongoose = require("mongoose")
  //ask fro password
 if(process.argv.length <3){
@@ -19,65 +18,16 @@ content: String,
 important: Boolean,
 })
 //create model(name of table, design to store)
-const Note1 = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
-// //cretae data
-// const note = new Note1({
-//   content:"THIS IS NOTE1",
-//   important: false,
-// })
-// //save data & close connection:
-// note.save().then(result=>{
-//   console.log("Note saved")
-//   mongoose.connection.close()
-// })
+//cretae data
+const note = new Note({
+  content:"THIS IS NOTE1",
+  important: false,
+})
+//save data & close connection:
+note.save().then(result=>{
+  console.log("Note saved")
+  mongoose.connection.close()
+})
 
-//retrieving data from database
-// Note1.find({}).then(resultNotes=>{
-//   resultNotes.map(note=>console.log(note))
-//   mongoose.connection.close();
-// })
-
-
-// //create list of data:
-// const notes = [
-//   {
-//     content: "1st obj",
-//     important: true
-//   },
-//   {
-//     content: "2nd obj",
-//     important: false
-//   }, {
-//     content: "3rd obj",
-//     important: true
-//   }, {
-//     content: "4th obj",
-//     important: false
-//   },
-// ]
-
-// //add note funtion (create single note 1st and then add to db)
-
-// function AddNote(content, important){
-//   const note = new Note1({
-//     content,
-//     important
-//   })
-//   note.save().then(result =>{
-//     console.log("note saved");
-//     console.log(result)
-//   })
-// }
-// //calling function number of array time
-// const promiseResults= notes.map(note=>{
-//   AddNote(note.content, note.important);
-
-// })
-// //check all promises fulfilled and then
-// promise.All(promiseResults)
-//   .then(response=>{
-//     console.log("All notes saved");
-//     mongoose.connection.close();
-
-//   })
